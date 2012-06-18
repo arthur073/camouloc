ea68d25f1d6252e1614f8cd9389632eec2580de1
Arthur Verger <arthur.verger@gmail.com>
2012-06-18 21:15:51 +0200

    migration

--- db/migrate/20120519094527_create_users.rb
+++ db/migrate/20120519094527_create_users.rb
@@ -1,15 +1,16 @@
 # -*- encoding : utf-8 -*-
 class CreateUsers < ActiveRecord::Migration
-  def change
-    create_table :users do |t|
-      t.string :nom
-      t.string :email
-      t.integer :colloc_id
+        def change
+                drop_table :users
+                create_table :users do |t|
+                        t.string :nom
+                        t.string :email
+                        t.integer :colloc_id
 
-      t.timestamps
-    end
-  end
-  def self.down
-    drop_table :users
-  end
+                        t.timestamps
+                end
+        end
+        def self.down
+                drop_table :users
+        end
 end
