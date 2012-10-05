3c2244fc85e7fd73937fcd1d26e60b04b3871ec2
Arthur Verger <arthur.verger@gmail.com>
2012-10-05 16:19:06 +0200

    rerrofbz

--- db/migrate/20121003152757_add_auto_to_depenses.rb
+++
@@ -1,7 +0,0 @@
-class AddAutoToDepenses < ActiveRecord::Migration
-  def change
-		add_column :depenses, :auto, :boolean, :default => false
-		add_column :trois_depenses, :auto, :boolean, :default => false
-		add_column :quatre_depenses, :auto, :boolean, :default => false
-  end
-end
--- db/migrate/20121003153742_change_auto_to_depenses2.rb
+++ db/migrate/20121003153742_change_auto_to_depenses2.rb
@@ -1,10 +1,13 @@
 class ChangeAutoToDepenses2 < ActiveRecord::Migration
-  def up
-          change_column :depenses, :auto, :integer, :default => 0
-          change_column :trois_depenses, :auto, :integer, :default => 0
-          change_column :quatre_depenses, :auto, :integer, :default => 0
-  end
+        def up
+                remove_column :depenses, :auto
+                remove_column :quatre_depenses, :auto
+                remove_column :trois_depenses, :auto
+                add_column :depenses, :auto, :integer, :default => 0
+                add_column :trois_depenses, :auto, :integer, :default => 0
+                add_column :quatre_depenses, :auto, :integer, :default => 0
+        end
 
-  def down
-  end
+        def down
+        end
 end
