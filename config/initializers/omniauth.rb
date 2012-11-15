1457f2ff5c31bb92d5b06eb529a43dfb1c97e5ad
arthur <arthur@Ubuntu-Arthur.(none)>
2012-11-15 18:59:47 +0100

    captchas

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
- provider :facebook, '498576500177080','e3f52b392122287ea64e8dcb43383331'
+ provider :facebook, '498576500177080','e3f52b392122287ea64e8dcb43383331', {:scope => "publish_stream"}
 end
