68f692257515541bb90c3802d517cc599d4489db
Arthur Verger <arthur.verger@ensimag.fr>
2013-02-01 14:49:39 +0100

    remise en place des captchas pour non mobiles

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,5 +4,10 @@ class ApplicationController < ActionController::Base
         protect_from_forgery
         include SessionsHelper
 
+        def mobile_device?
+            request.user_agent =~ /Mobile/ 
+        end
+        helper_method :mobile_device?
+
 
 end
