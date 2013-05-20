d4464c90cdb7b45cf22db9a1d65e4e7386b32959
arthur <arthur.verger@gmail.com>
2013-05-20 12:27:19 +0200

    ok to push

--- app/assets/javascripts/jquery.eislideshow.js
+++ app/assets/javascripts/jquery.eislideshow.js
@@ -108,7 +108,7 @@
 			
 			// preload images
 			// add loading status
-			this.$loading		= $('<div class="ei-slider-loading">Loading</div>').prependTo( _self.$el );
+			this.$loading		= $('<div class="ei-slider-loading">Loading...</div>').prependTo( _self.$el );
 			
 			$.when( this._preloadImages() ).done( function() {
 				
