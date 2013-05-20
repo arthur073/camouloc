6dbb0cf0e90f471a748e585b1a0d2d282be65989
arthur <arthur.verger@gmail.com>
2013-05-20 17:41:48 +0200

    internationnalisation part1

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,18 +1,39 @@
 # -*- encoding : utf-8 -*-
 class ApplicationController < ActionController::Base
 
-        protect_from_forgery
-        include SessionsHelper
+	protect_from_forgery
+	include SessionsHelper
 
-        before_filter :prepare_for_mobile
+	before_filter :prepare_for_mobile
+	before_filter :set_user_language
+	before_filter :set_locale
 
-        def mobile_device?
-            request.user_agent =~ /Mobile/ 
-        end
-        helper_method :mobile_device?
+	def mobile_device?
+		request.user_agent =~ /Mobile/ 
+	end
+	helper_method :mobile_device?
 
-        def prepare_for_mobile
-           request.format = :mobile if mobile_device?
-        end
+	def prepare_for_mobile
+		request.format = :mobile if mobile_device?
+	end
+
+	def set_locale
+		if params[:locale].blank?
+			I18n.locale = 'fr'
+		else
+			I18n.locale = params[:locale]
+		end
+	end   
+
+	# ensure locale persists
+	def default_url_options(options={})
+		{:locale => I18n.locale}
+	end
+
+	private
+
+	def set_user_language
+		I18n.locale = 'fr'
+	end
 
 end
