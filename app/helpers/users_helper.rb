0b8fe09377aa1f94db1cd78436b890966eb81529
arthur <arthur.verger@gmail.com>
2013-05-20 12:37:22 +0200

    ok to push

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,7 +1,7 @@
 # -*- encoding : utf-8 -*-
 module UsersHelper
 	def gravatar_for(user, options = { :size => 85 })
-		image_tag("../unknown.jpg", :alt => "unknown", :class => "imgframe", :size => "100x100")
+		image_tag("../unknown.jpg", :alt => "unknown", :class => "imgframeProfil", :size => "100x100")
 	end
 
 	def small_gravatar_for(user, options = { :size => 85 })
