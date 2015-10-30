eb0fed970978d034e4279b8a9985111b3e4b3dca
Arthur <arthur.verger@gmail.com>
2015-10-30 17:02:28 +0100

    cedar 14

--- db/migrate/20121003153742_change_auto_to_depenses2.rb
+++ db/migrate/20121003153742_change_auto_to_depenses2.rb
@@ -1,6 +1,6 @@
 class ChangeAutoToDepenses2 < ActiveRecord::Migration
         def up
-                remove_column :depenses, :auto
+                #remove_column :depenses, :auto
                 remove_column :quatre_depenses, :auto
                 remove_column :trois_depenses, :auto
                 add_column :depenses, :auto, :integer, :default => 0
