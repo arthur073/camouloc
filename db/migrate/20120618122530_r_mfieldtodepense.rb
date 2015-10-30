a660dbb781cc54d45f5cbe54ef8025ad89712fb8
Arthur <arthur.verger@gmail.com>
2015-10-30 17:00:55 +0100

    cedar 14

--- db/migrate/20120618122530_r_mfieldtodepense.rb
+++ db/migrate/20120618122530_r_mfieldtodepense.rb
@@ -2,7 +2,7 @@ class RMfieldtodepense < ActiveRecord::Migration
   def up
         remove_column :depenses, :destinataire_part2
         remove_column :trois_depenses, :destinataire_part3
-        remove_column :quatre_depenses, :destinataire_part4
+        #remove_column :quatre_depenses, :destinataire_part4
   end
 
   def down
